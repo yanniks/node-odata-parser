7168754592e7c432c8aa1588715aea6389c1ff59
Henning Pohlhausen <henning.pohlhausen@claas.com>
2020-09-02 15:55:57 +0200

    Add type declaration

---
+++ lib/index.d.ts
@@ -0,0 +1,148 @@
+export class SyntaxError {
+    constructor(expected: any, found: any, offset: any, line: any, column: any);
+
+    // Native method; no parameter or return type inference available
+    public toString(): any;
+
+}
+
+export enum OdataParserRule {
+    WSP,
+    DIGIT,
+    INT,
+    HEXDIG,
+    HEXDIG2,
+    HEXDIG4,
+    HEXDIG8,
+    SQUOTE,
+    primitiveLiteral,
+    null,
+    binary,
+    boolean,
+    byte,
+    dateTime,
+    dateTimeOffset,
+    dateTimeBodyA,
+    dateTimeBodyB,
+    dateTimeBodyC,
+    dateTimeBodyD,
+    dateTimeBody,
+    dateTimeOffsetBody,
+    decimal,
+    double,
+    guid,
+    int32,
+    int64,
+    sbyte,
+    single,
+    string,
+    oneToNine,
+    zeroToTwelve,
+    zeroToThirteen,
+    zeroToSixty,
+    zeroToThirtyOne,
+    zeroToTwentyFour,
+    year,
+    sign,
+    nan,
+    negativeInfinity,
+    positiveInfinity,
+    nanInfinity,
+    unreserved,
+    validstring,
+    escapedQuote,
+    identifierPart,
+    identifier,
+    callback,
+    top,
+    expand,
+    expandList,
+    skip,
+    format,
+    inlinecount,
+    orderby,
+    orderbyList,
+    select,
+    identifierPathParts,
+    identifierPath,
+    selectList,
+    filter,
+    filterExpr,
+    booleanFunctions2Args,
+    booleanFunc,
+    otherFunctions1Arg,
+    otherFunc1,
+    otherFunctions2Arg,
+    otherFunc2,
+    cond,
+    part,
+    op,
+    unsupported,
+    expList,
+    exp,
+    query,
+}
+
+export enum IFilterOperation {
+    logicalAnd = "and",
+    logicalOr = "or",
+    logicalNegation = "not",
+    equal = "eq",
+    notEqual = "ne",
+    lessThan = "lt",
+    lessThanOrEqual = "le",
+    greaterThan = "gt",
+    greatherThanOrEqual = "ge",
+    addition = "add",
+    substraction = "sub",
+    multiplication = "mul",
+    division = "div",
+    modulo = "mod",
+}
+
+export interface IFilterFunctionCall {
+    type: "functioncall";
+    func: string;
+    args: IFilterValueProperty[] | IFilterValueLiteral[];
+}
+
+export interface IFilterValueProperty {
+    type: "property";
+    name?: string;
+}
+
+export interface IFilterValueLiteral {
+    type: "literal";
+    value?: string;
+}
+
+export interface IFilterComparison {
+    type: IFilterOperation;
+    left: IFilterValueProperty | IFilterValueLiteral | IFilterComparison | IFilterFunctionCall;
+    right: IFilterValueProperty | IFilterValueLiteral | IFilterComparison | IFilterFunctionCall;
+}
+
+export interface IODataProperties {
+    $filter?: IFilterComparison | IFilterFunctionCall;
+    $top?: number;
+    $skip?: number;
+    $select?: string[];
+    $search?: string;
+    $inlinecount?: any;
+    $callback?: any;
+    $expand?: string[];
+    $orderby?: string[];
+}
+
+export function parse(input: string, startRule?: OdataParserRule): IODataProperties;
+
+export function toSource(): any;
+
+export namespace parse {
+    const prototype: {};
+
+}
+
+export namespace toSource {
+    const prototype: {};
+}
